82532838fc5a666511024c1eb90b38759b98dd4c
asioben <89880816+asioben@users.noreply.github.com>
2025-11-14 22:39:16 -0500

    asioben commit
    asioben commit

--- Prelim1/part3/part_3.py
+++ Prelim1/part3/part_3.py
@@ -1,27 +1,62 @@
-def part_3(phrase):
-    mot = "ornithorynque"
-    liste_lettre = []
-    depart = 0
+# For your imports look at the rulebook to see what is allowed
+# Pour les import permis allez vous référer au livre des règlements
+"""part_3.py
+This is the template file for the part 3 of the Prelim 1.
+Ceci est le fichier template pour la partie 3 du Prelim 1.
+"""
+
+def string_to_array(phrase: str):
+    array = []
+    for letter in phrase:
+        array.append(letter)
+    return array
+
+def find(array: list, letter):
+    j = -1
+    k = 0
+    condition = True
+    for i in array:
+        j += 1
+        if(i == letter):
+            condition = False
+            k = j
+            break
+    if(condition == True):
+        j = -1
+    if(condition == False):
+        array[k] = ''
+    return j
+
+def part_3(sentence: str):
+    """
+    Find "ornithorynque" in the sentence
+
+    Parameters:
+        sentence str: The sentence that may contain contain the letters of "ornithorynque"
+
+    Returns:
+        [int]: The position of the letters found
+    """
+    word = "ornithorynque"
+    #phrase = sentence
+    positions = []
+
+    phrase = string_to_array(sentence)
+
+    for letter in word:
+        position = find(phrase,letter)
+        positions.append(position)
+   
+
+    print(positions)
+    print(phrase)
+    print(sentence)
     ### You code goes here ###
     ### Votre code va ici ###
+    
+    
 
-    def verification(position, liste):
-        for position_liste in liste:
-            if position_liste == position:
-                return False
-        else:
-            return True
 
-    for lettre in mot:
-        position_lettre_mot = phrase.find(lettre, depart)
+    return positions
 
-        if len(liste_lettre) > 0:
-            for _ in range(len(phrase)):
-                if not verification(position_lettre_mot, liste_lettre):
-                    depart += 1
-                    position_lettre_mot = phrase.find(lettre, depart)
-                else:
-                    break
-        liste_lettre.append(position_lettre_mot)
-        depart = 0
-    return liste_lettre
+part_3("What exactly is that thing called an ornithorynque in french??")
